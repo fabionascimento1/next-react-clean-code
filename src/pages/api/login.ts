4abfc752172392e5152a2866c7b3e54a9fbf83bc
Fabio Nascimento <fabionascimento@smartbr.com>
2023-01-24 17:10:28 -0300

    feat: api login setHeader and typing

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,7 +2,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
+  access_token: string;
+  expires_in: number;
+  userId: number;
+  menus: any[];
   name: string;
+  professionalId: number;
+  refresh_token: string;
+  roles: any[];
+  username: string;
 };
 
 export default function handler(
@@ -10,12 +18,14 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === "POST") {
-    console.log(req.body);
+    // console.log(req.body);
+    const token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImF1ZCI6IjExNCIsImlzcyI6Imh0dHBzOi8vdGVzdGUudml0YXZlcnNlLmNvbS5iciIsImV4cCI6MTY3NDYxNTQ5MiwiaWF0IjoxNjc0NTg2NjkyLCJhdXRob3JpdGllcyI6WyJST0xFX0FETUlOIl19.iwqwCenVFlEEpwDpuu55t9YQsbxNl2AhZVpQFhrnLFM";
+    res.setHeader("Authorization", token);
     res.status(200).json({
-      access_token:
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImF1ZCI6IjExNCIsImlzcyI6Imh0dHBzOi8vdGVzdGUudml0YXZlcnNlLmNvbS5iciIsImV4cCI6MTY3NDYxNTQ5MiwiaWF0IjoxNjc0NTg2NjkyLCJhdXRob3JpdGllcyI6WyJST0xFX0FETUlOIl19.iwqwCenVFlEEpwDpuu55t9YQsbxNl2AhZVpQFhrnLFM",
+      access_token: token,
       expires_in: 1674615492758,
-      idUsuario: 39,
+      userId: 39,
       menus: [
         { itens: [], nome: "Home", icone: "Home", tipo: "main", key: "/" },
 
@@ -52,7 +62,7 @@ export default function handler(
         },
       ],
       name: "Admin",
-      professional: 38,
+      professionalId: 38,
       refresh_token:
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY3NDYxNjM5MiwianRpIjoiMjg3ZGE3ZTUtM2I2OC00OTI0LTg0NmEtNjI2NTk1MjEyODA1In0.92JtS5legaNTz1P0KlXWJV4-ynfJAOfAWMFgCQYIASI",
       roles: ["ROLE_ADMIN"],
